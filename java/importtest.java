1b3ccbb6cc00541bffb127439e8d52e2ba01c551
ks0888 <108438184+k-s88@users.noreply.github.com>
2022-06-30 21:28:33 +0900

    Add files via upload

---
+++ java/importtest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class importtest {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(str + "‚Æ“ü—Í‚³‚ê‚Ü‚µ‚½");
+        sc.close();
+    }
+}
