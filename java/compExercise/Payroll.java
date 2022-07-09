ddd8b343f201c4da5a5772e2712afa4f22de08af
ks0888 <108438184+k-s88@users.noreply.github.com>
2022-07-09 09:28:19 +0900

    Update Payroll.java

--- java/compExercise/Payroll.java
+++ java/compExercise/Payroll.java
@@ -21,33 +21,33 @@ public class Payroll {
 		int workingHour = (int)( workingTime / ONE_HOUR_BY_MILLI_SEC );                      // 時間に換算
 		int workingMin  = (int)( ( workingTime / ONE_MIN_BY_MILLI_SEC ) % ONE_HOUR_BY_MIN ); // 分に換算
 	
-	// 以下課題部分
-        // 給与計算
-        final int wage = 15 ; // 1分あたりの給与
-        final int restTime1 = 45 ; // 労働時間が6～8時間の場合の休憩時間
-        final int restTime2 = 60 ; // 労働時間が8時間以上の場合の休憩時間
-        final double excessMag = 1.25 ; // 超過分の倍率
-        final int legalWorkingHours = 480 ; // 法定労働時間
+		// 以下課題部分
+       		// 給与計算
+        	final int wage = 15 ; // 1分あたりの給与
+        	final int restTime1 = 45 ; // 労働時間が6～8時間の場合の休憩時間
+        	final int restTime2 = 60 ; // 労働時間が8時間以上の場合の休憩時間
+        	final double excessMag = 1.25 ; // 超過分の倍率
+        	final int legalWorkingHours = 480 ; // 法定労働時間
         
-        int workingTimeConv = workingHour * 60 + workingMin ; //労働時間を分単位で合計
-        int TotalWage = 0; 
+        	int workingTimeConv = workingHour * 60 + workingMin ; //労働時間を分単位で合計
+        	int TotalWage = 0; 
 
-        if(workingTimeConv <= 360){
-            TotalWage =  wage * workingTimeConv;
+        	if(workingTimeConv <= 360){
+            		TotalWage =  wage * workingTimeConv;
 
-        }else if(workingTimeConv > 360 && workingTimeConv <= 480){
-            TotalWage =  wage * (workingTimeConv - restTime1);
+        	}else if(workingTimeConv > 360 && workingTimeConv <= 480){
+            		TotalWage =  wage * (workingTimeConv - restTime1);
 
-        }else if(workingTimeConv >= 480 && workingTimeConv < 540){
-            TotalWage =  wage * (workingTimeConv - restTime2);
+        	}else if(workingTimeConv >= 480 && workingTimeConv < 540){
+            		TotalWage =  wage * (workingTimeConv - restTime2);
         
-        }else if(workingTimeConv > 540){
-            int overtime = workingTimeConv - 540 ;
-            TotalWage =  (wage * legalWorkingHours) + (wage * (int)(excessMag) * overtime) ;
-        }
+        	}else if(workingTimeConv > 540){
+            		int overtime = workingTimeConv - 540 ;
+            		TotalWage =  (wage * legalWorkingHours) + (wage * (int)(excessMag) * overtime) ;
+        	}
 
         
-        // 出力
+        	// 出力
 		System.out.println("本日の給与は" + TotalWage + "円です。");
 	}
 }
